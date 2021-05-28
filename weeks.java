4f4803bf59bd546048c2265b7819cc3a5e1cf4cb
Premkumar05012000 <83026647+Premkumar05012000@users.noreply.github.com>
2021-05-28 17:24:49 +0530

    Create weeks.java

---
+++ weeks.java
@@ -0,0 +1,35 @@
+package com.assignment;
+
+public class SwitchClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="sunday";
+		switch (str) {
+		case "sunday":
+			System.out.println("First day of the week");
+			break;
+		case "saturday":
+			System.out.println("Second day of the week");
+			break;
+		case "monday":
+			System.out.println("Third day of the week");
+			break;
+		case "tuesday":
+			System.out.println("Fourth day of the week");
+			break;
+		case "wednesday":
+			System.out.println("Fifth day of the week");
+			break;
+		case "thursday":
+			System.out.println("Sixth day of the week");
+			break;
+		case "friday":
+			System.out.println("Seventh day of the week");
+			break;
+			
+		}
+
+	}
+
+}
